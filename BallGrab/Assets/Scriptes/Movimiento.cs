1abfc93a3942ce0753b2a561a1c0437a856ef540
Kevin Carreño <kevincarrenot@gmail.com>
2020-10-15 13:28:04 -0500

    Escena

---
+++ BallGrab/Assets/Scriptes/Movimiento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+   
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed,0);
+    }
+}
